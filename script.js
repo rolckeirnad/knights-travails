adbb207826182b0f4b865ff4baff748f4048ff7c
Daniel Gallardo <dgallardo.c@outlook.com>
2022-09-16 20:07:47 -0500

    Define node structure

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function gameBoard() {
   let row = Array(8).fill().map((_, i) => i);
-  let board = row.map((v, _, a) => a.map((_, i) => `[${v},${i}]`));
+  let board = row.map((v, _, a) => a.map((_, i) => [v, i]));
   return board;
 }
+
+const Node = (data) => {
+  const node = {
+    data,
+    a: null,
+    b: null,
+    c: null,
+    d: null,
+    e: null,
+    f: null,
+    g: null,
+    h: null,
+  };
+  return node;
+};
 
 function knight(position, board) {
 }
