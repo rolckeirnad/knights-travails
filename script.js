8add356df2ab64d933db31cf7f00e7cae58417e4
Daniel Gallardo <dgallardo.c@outlook.com>
2022-09-18 01:40:06 -0500

    Add Knight function - This function will create a tree with interconnected nodes from   knight's possible movements.

--- script.js
+++ script.js
@@ -4,20 +4,38 @@ function gameBoard() {
   return board;
 }
 
-const Node = (data) => {
-  const node = {
-    data,
-    a: null,
-    b: null,
-    c: null,
-    d: null,
-    e: null,
-    f: null,
-    g: null,
-    h: null,
+const Knight = (board) => {
+  let tree = null;
+  if (board) buildBoardTree(board);
+
+  function buildBoardTree(board) {
+    // This node conditions define all the possible movements of knight.
+    const Node = () => ({
+      a: { offset: [1, 2], condition: (x, y) => (x + 1 < 8 && y + 2 < 8) },
+      b: { offset: [2, 1], condition: (x, y) => (x + 2 < 8 && y + 1 < 8) },
+      c: { offset: [2, -1], condition: (x, y) => (x + 2 < 8 && y - 1 >= 0) },
+      d: { offset: [1, -2], condition: (x, y) => (x + 1 < 8 && y - 2 >= 0) },
+      e: { offset: [-1, -2], condition: (x, y) => (x - 1 >= 0 && y - 2 >= 0) },
+      f: { offset: [-2, -1], condition: (x, y) => (x - 2 >= 0 && y - 1 >= 0) },
+      g: { offset: [-2, 1], condition: (x, y) => (x - 2 >= 0 && y + 1 < 8) },
+      h: { offset: [-1, 2], condition: (x, y) => (x - 1 >= 0 && y + 2 < 8) },
+    });
+    // We build full tree of board with all knight possible movements.
+    tree = setBoardTree(board, Node());
+  }
+  return tree;
 };
-  return node;
-};
+
+// This updates the board objects adding necessary keys.
+function setBoardTree(board, typeOfNode) {
+  board.forEach((row, x) => row.forEach((node, y) => {
+    for (let key in typeOfNode) {
+      [offX, offY] = typeOfNode[key].offset;
+      node[key] = typeOfNode[key].condition(x, y) ? board[x + offX][y + offY] : null;
+    }
+  }));
+  return board;
+}
 
 function knight(position, board) {
 }
